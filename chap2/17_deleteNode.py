9f92a9655b321099d95ce05788653e74eb4758d7
gdutthu <2194454302@qq.com>
2020-08-11 21:25:14 +0800

    Create 17_deleteNode.py

---
+++ chap2/17_deleteNode.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        if node.next==None:   #要删除的节点为尾节点
+            node=None
+        else:                 #要删除的节点为中间节点
+            node.val=node.next.val
+            node.next=node.next.next
