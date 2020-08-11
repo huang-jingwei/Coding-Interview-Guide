22d7c6aadf41185537b24fe07626f7f0558f0432
gdutthu <2194454302@qq.com>
2020-08-11 17:09:08 +0800

    Update 01_printCommonPart.py

--- chap2/01_printCommonPart.py
+++ chap2/01_printCommonPart.py
@@ -1,5 +1,22 @@
+def printCommonPart(array1,array2):
+    data=[]
+    index1=0
+    index2=0
+    while index1 <len(array1) and index2 <len(array2):
+        if array1[index1]==array2[index2]:
+            data.append(array1[index1])
+            index1 +=1
+            index2 +=1
+        elif array1[index1]<array2[index2]:
+            index1 +=1
+        elif array1[index1]>array2[index2]:
+            index2 +=1
+    return data
 
 
-
-def printCommonPart():
-    pass
+n = int(input())
+array1 = list(map(int,input().strip().split()))
+m = int(input())
+array2 = list(map(int,input().strip().split()))
+data=printCommonPart(array1, array2)
+print(data)
