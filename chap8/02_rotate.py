2e2ba1c61a451fbc5dfc8adea1b16092e0e9968a
gdutthu <2194454302@qq.com>
2020-08-15 15:41:58 +0800

    Update 02_rotate.py

--- chap8/02_rotate.py
+++ chap8/02_rotate.py
@@ -1,32 +1,31 @@
 class Solution:
-    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        # 若输入为空或者输入矩阵为行向量，则返回空
-        if matrix == None or len(matrix) < 1:
-            return []
-        array = list()  # 空数组，用来存放矩阵顺时针打印过程中的数字
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        rows = len(matrix)  # 矩阵的行、列
+        cols = len(matrix[0])
+        if rows == 0 or rows == 1:
+            return matrix
 
-        leftRow = 0     # 打印区域的左上角，右下角端点坐标
+        leftRow = 0         # 移动区域的左上角、右下角
         leftCol = 0
-        rightRow = len(matrix) - 1
-        rightCol = len(matrix[0]) - 1
+        rightRow = rows - 1
+        rightCol = cols - 1
 
-        while True:
+        while leftRow < rightRow:
+            self.rotate_process(matrix, leftRow, leftCol, rightRow, rightCol)
+            leftRow += 1        # 更新端点坐标
+            leftCol += 1
+            rightRow -= 1
+            rightCol -= 1
+        return matrix
 
-            # 按照四次过程来获取顺时针打印过程的数值
-            for index in range(leftCol, rightCol + 1): array.append(matrix[leftRow][index])
-            leftRow = leftRow + 1    # 更新左上角端点行坐标
-            if leftRow > rightRow: break
-
-            for index in range(leftRow, rightRow + 1): array.append(matrix[index][rightCol])
-            rightCol = rightCol - 1   # 更新右下角端点列坐标
-            if leftCol > rightCol: break
-
-            for index in range(rightCol, leftCol - 1, -1): array.append(matrix[rightRow][index])
-            rightRow = rightRow - 1   # 更新右下角端点行坐标
-            if leftRow > rightRow: break
-
-            for index in range(rightRow, leftRow - 1, -1): array.append(matrix[index][leftCol])
-            leftCol = leftCol + 1     # 更新左上角端点的列坐标
-            if leftCol > rightCol: break
-
-        return array
+    def rotate_process(self, matrix, leftRow, leftCol, rightRow, rightCol):
+        times = rightCol - leftCol
+        for i in range(times):
+            item = matrix[leftRow][leftCol + i]
+            matrix[leftRow][leftCol + i] = matrix[rightRow - i][leftCol]
+            matrix[rightRow - i][leftCol] = matrix[rightRow][rightCol - i]
+            matrix[rightRow][rightCol - i] = matrix[leftRow + i][rightCol]
+            matrix[leftRow + i][rightCol] = item
