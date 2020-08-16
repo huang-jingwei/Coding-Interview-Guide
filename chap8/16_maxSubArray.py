c899faeb162c2d6ce2a36cd8a1b3f6438cc98a24
gdutthu <2194454302@qq.com>
2020-08-16 16:26:50 +0800

    Create 16_maxSubArray.py

---
+++ chap8/16_maxSubArray.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        if len(nums) == 1:  # 只有一个元素
+            return nums[0]
+
+        maxSubArraySum = [0] * len(nums)
+        for index in range(len(maxSubArraySum)):
+            if index == 0:
+                maxSubArraySum[index] = nums[index]
+            elif index > 0:
+                if maxSubArraySum[index - 1] >= 0:
+                    maxSubArraySum[index] = maxSubArraySum[index - 1] + nums[index]
+                else:
+                    maxSubArraySum[index] = nums[index]
+        return max(maxSubArraySum)
